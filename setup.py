c8797aa4895b121b929e85967e14c6252454b7b4
Syrwegian <mnkanout@gmail.com>
2020-03-13 17:15:45 +0100

    fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysafe",
-    version="0.1.2",
+    version="0.1.3",
     author="MNKANOUT",
     author_email="mnkanout@gmail.com",
     description="Python password manger",
