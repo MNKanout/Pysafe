da5f9e4d5c80b19bc44d37837e276ab4b62ec4a7
Syrwegian <mnkanout@gmail.com>
2020-03-15 13:51:43 +0100

    Password fix for windows

--- src/password.py
+++ src/password.py
@@ -14,15 +14,15 @@ def generate_password():
             continue
         else:
             custom_punctuation = "!#$%*+-?@_"
-            password_mix = string.ascii_letters + string.digits\
-                           + custom_punctuation
+            password_mix = string.ascii_letters+string.digits+custom_punctuation
             p = []
             i = 0
             while i <= length:
                 p.append(random.choice(password_mix))
                 i = i + 1
-            separator = ''
-            print(f'\nNew password:{Fore.LIGHTYELLOW_EX + separator.join(p)}\n')
+            separator =''
+            print(f'\nNew password:'
+                  f'{Fore.LIGHTYELLOW_EX+str(separator.join(p))}\n')
             while True:
                 print(Fore.LIGHTRED_EX + 'Regenerate password?(y/n)')
                 regenerate = input()
